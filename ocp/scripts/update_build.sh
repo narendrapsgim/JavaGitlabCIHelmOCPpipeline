a7c2948068f250485ecfa850ac455726441b7529
uniwinux <uniwinux@gmail.com>
2024-09-10 02:34:14 +0000

    test

--- ocp/scripts/update_build.sh
+++ ocp/scripts/update_build.sh
@@ -2,26 +2,24 @@
 
 # update the templates        
 # declare an array variable
-declare -a arr=('ocp/build/buildconfig1.yaml' 'ocp/build/buildconfig2.yaml' 'ocp/build/rbac-helm.yaml' 'ocp/build/vault.yaml')
+declare -a arr=("ocp/build/buildconfig1.yaml" "ocp/build/buildconfig2.yaml" "ocp/build/rbac-helm.yaml")
 ## now loop through the above array
-for i in '${arr[@]}'
+for i in "${arr[@]}"
 do
-    s_replace 'xNAMESPACEx' '$NAMESPACE' '$i'
-    s_replace 'xNAMESPACE_RUNNERx' '$NAMESPACE_RUNNER' '$i'
-    s_replace 'xNAMEx' '$GRADLE_NAME' '$i'
-    s_replace 'xPULLSECRETx' '$SECRET1NAME' '$i'
-    s_replace 'xAPPVERSIONx' 'latest' '$i'
-    s_replace 'xGRADLE_BINx' '$GRADLE_NAME-$GRADLE_VERSION.$GRADLE_EXT' '$i'
-    s_replace 'xTIMEZONEREGIONx' '$TIMEZONEREGION' '$i'
-    s_replace 'xTIMEZONECITYx' '$TIMEZONECITY' '$i'
-    s_replace 'xREGISTRYx' '$REGISTRY' '$i'
-    s_replace 'xREPOSITORYx' '$REPOSITORY' '$i'
-    s_replace 'xTAGx' '$TAG' '$i'
-    s_replace 'xENV_CONTEXTx' '$GRADLE_NAME' '$i'
-    s_replace 'xDOCKER_USERx' '$DOCKER_USER' '$i'      
-    s_replace 'xBRANCHx' '$BRANCH' '$i'  
-    s_replace 'xMAIN_GROUPx' '$MAIN_GROUP' '$i' 
-    s_replace 'xNAME_REPOx' '$NAME_REPO' '$i' 
-    s_replace 'xSECRET1NAMEx' '$SECRET1NAME' '$i'
-    s_replace 'xSECRET1MOUNTx' '$SECRET1MOUNT' '$i'
+    s_replace "xNAMESPACEx" "$NAMESPACE" "$i"
+    s_replace "xNAMESPACE_RUNNERx" "$NAMESPACE_RUNNER" "$i"
+    s_replace "xNAMEx" "$GRADLE_NAME" "$i"
+    s_replace "xPULLSECRETx" "$SECRET1NAME" "$i"
+    s_replace "xAPPVERSIONx" "latest" "$i"
+    s_replace "xGRADLE_BINx" "$GRADLE_NAME-$GRADLE_VERSION.$GRADLE_EXT" "$i"
+    s_replace "xTIMEZONEREGIONx" "$TIMEZONEREGION" "$i"
+    s_replace "xTIMEZONECITYx" "$TIMEZONECITY" "$i"
+    s_replace "xREGISTRYx" "$REGISTRY" "$i"
+    s_replace "xREPOSITORYx" "$REPOSITORY" "$i"
+    s_replace "xTAGx" "$TAG" "$i"
+    s_replace "xENV_CONTEXTx" "$GRADLE_NAME" "$i"
+    s_replace "xDOCKER_USERx" "$DOCKER_USER" "$i"      
+    s_replace "xBRANCHx" "$BRANCH" "$i"  
+    s_replace "xMAIN_GROUPx" "$MAIN_GROUP" "$i" 
+    s_replace "xNAME_REPOx" "$NAME_REPO" "$i" 
 done
--- ocp/scripts/update_helm.sh
+++ ocp/scripts/update_helm.sh
@@ -2,22 +2,22 @@
 
 # update the templates        
 # declare an array variable
-declare -a arr=('ocp/helm/Chart.yaml' 'ocp/helm/values.yaml')
+declare -a arr=("ocp/helm/Chart.yaml" "ocp/helm/values.yaml")
 ## now loop through the above array
-for i in '${arr[@]}'
+for i in "${arr[@]}"
 do
-    s_replace 'xNAMESPACEx' '$NAMESPACE' '$i'
-    s_replace 'xNAMEx' '$GRADLE_NAME' '$i'
-    s_replace 'xPULLSECRETx' '$SECRET1NAME' '$i'
-    s_replace 'xCHARTVERSIONx' '$CHARTVERSION' '$i'
-    s_replace 'xAPPVERSIONx' 'latest' '$i'
-    s_replace 'xGRADLE_BINx' '$GRADLE_NAME-$GRADLE_VERSION.$GRADLE_EXT' '$i'
-    s_replace 'xREGISTRYx' '$REGISTRY' '$i'
-    s_replace 'xREPOSITORYx' '$REPOSITORY' '$i'
-    s_replace 'xTAGx' '$TAG' '$i'
-    s_replace 'xDOCKER_USERx' '$DOCKER_USER' '$i'      
-    s_replace 'xPORTx' '$PORT' '$i' 
-    s_replace 'xHOSTNAMEx' '$HOSTNAME' '$i'
-    s_replace 'xSECRET1NAMEx' '$SECRET1NAME' '$i'
-    s_replace 'xSECRET1MOUNTx' '$SECRET1MOUNT' '$i'
+    s_replace "xNAMESPACEx" "$NAMESPACE" "$i"
+    s_replace "xNAMEx" "$GRADLE_NAME" "$i"
+    s_replace "xPULLSECRETx" "$SECRET1NAME" "$i"
+    s_replace "xCHARTVERSIONx" "$CHARTVERSION" "$i"
+    s_replace "xAPPVERSIONx" "latest" "$i"
+    s_replace "xGRADLE_BINx" "$GRADLE_NAME-$GRADLE_VERSION.$GRADLE_EXT" "$i"
+    s_replace "xREGISTRYx" "$REGISTRY" "$i"
+    s_replace "xREPOSITORYx" "$REPOSITORY" "$i"
+    s_replace "xTAGx" "$TAG" "$i"
+    s_replace "xDOCKER_USERx" "$DOCKER_USER" "$i"      
+    s_replace "xPORTx" "$PORT" "$i" 
+    s_replace "xHOSTNAMEx" "$HOSTNAME" "$i"
+    s_replace "xSECRET1NAMEx" "$SECRET1NAME" "$i"
+    s_replace "xSECRET1MOUNTx" "$SECRET1MOUNT" "$i"
 done
